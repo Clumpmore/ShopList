7178b8d0a9ca691243956990b3007bec2c867342
Clumpmore <ivershinin21@gmail.com>
2023-03-09 20:54:16 +0300

    added presentation lay

--- app/src/main/java/com/example/shoplist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoplist/domain/ShopItem.kt
@@ -3,7 +3,7 @@ package com.example.shoplist.domain
 data class ShopItem(
     val name: String,
     val count: Int,
-    val Enabled: Boolean,
+    val enabled: Boolean,
     var id: Int = UNDEFINED_ID
 )
 {
